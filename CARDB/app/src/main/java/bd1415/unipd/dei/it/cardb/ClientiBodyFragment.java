ca5cdaf1a0f7fe6defb1aaa07c158dd983c3cba4
MrBonty <bontempo.omar@gmail.com>
2015-04-23 13:48:50 +0200

    To pull

--- CARDB/app/src/main/java/bd1415/unipd/dei/it/cardb/ClientiBodyFragment.java
+++ CARDB/app/src/main/java/bd1415/unipd/dei/it/cardb/ClientiBodyFragment.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ClientiBodyFragment extends Fragment {
+
+    private ViewHolder viewHolder;
 
     //onCreate
     @Override
@@ -24,6 +29,38 @@ public class ClientiBodyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.clienti_body_fragment, container, false);
+
+        viewHolder = null;
+        if (view == null) {
+            viewHolder.nome = (TextView) view.findViewById(R.id.cliente_nome_data);
+            viewHolder.cognomeLayout = (LinearLayout) view.findViewById(R.id.cliete_cognome_layout);
+            viewHolder.cognome = (TextView) view.findViewById(R.id.cliente_cognome_data);
+            viewHolder.pkTag = (TextView) view.findViewById(R.id.cliente_pk_tag);
+            viewHolder.pk = (TextView) view.findViewById(R.id.cliente_nome_tag);
+            viewHolder.telefono = (TextView) view.findViewById(R.id.cliente_telefono_data);
+            viewHolder.citta = (TextView) view.findViewById(R.id.cliente_citta_data);
+            viewHolder.provincia = (TextView) view.findViewById(R.id.cliente_provincia_data);
+            viewHolder.indirizzo = (TextView) view.findViewById(R.id.cliente_indirizzo_data);
+            viewHolder.veicoli = (ListView) view.findViewById(android.R.id.list);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
         return view;
+    }
+
+
+    private class ViewHolder {
+        public TextView nome;
+        public LinearLayout cognomeLayout;
+        public TextView cognome;
+        public TextView pkTag;
+        public TextView pk;
+        public TextView telefono;
+        public TextView citta;
+        public TextView provincia;
+        public TextView indirizzo;
+        public ListView veicoli;
     }
 }
--- CARDB/app/src/main/java/bd1415/unipd/dei/it/cardb/VeicoliBodyFragment.java
+++ CARDB/app/src/main/java/bd1415/unipd/dei/it/cardb/VeicoliBodyFragment.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class VeicoliBodyFragment extends Fragment {
+
+    private ViewHolder viewHolder;
 
     //onCreate
     @Override
@@ -24,6 +28,32 @@ public class VeicoliBodyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.veicoli_body_fragment, container, false);
+
+        viewHolder = null;
+        if (view == null) {
+            viewHolder.targa = (TextView) view.findViewById(R.id.veicolo_targa_data);
+            viewHolder.numero_telaio = (TextView) view.findViewById(R.id.veicolo_telaio_data);
+            viewHolder.proprietario = (TextView) view.findViewById(R.id.veicolo_proprietario_data);
+            viewHolder.marca = (TextView) view.findViewById(R.id.veicolo_modello_marca_data);
+            viewHolder.modello = (TextView) view.findViewById(R.id.veicolo_modello_nome_data);
+            viewHolder.anno_modello = (TextView) view.findViewById(R.id.veicolo_modello_anno_data);
+            viewHolder.lavorazioni = (ListView) view.findViewById(android.R.id.list);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+
         return view;
+    }
+
+    private class ViewHolder {
+        public TextView targa;
+        public TextView numero_telaio;
+        public TextView proprietario;
+        public TextView marca;
+        public TextView modello;
+        public TextView anno_modello;
+        public ListView lavorazioni;
     }
 }
